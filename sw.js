dccf17ecf87b4bad82864ede0e16da6ea3e092d7
yko155375-code <yko155375@gmail.com>
2026-05-24 17:36:13 +0800

    Add service worker

---
+++ sw.js
@@ -0,0 +1,19 @@
+const cacheName = "trpg-assistant-cache-v25";
+const filesToCache = ["./", "index.html", "styles.css", "app.js", "manifest.webmanifest", "assets/icon.svg", "assets/scene-gate.svg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(filesToCache)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key)))));
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+    const copy = response.clone();
+    caches.open(cacheName).then((cache) => cache.put(event.request, copy));
+    return response;
+  })));
+});
