1c2b4c2725a57115df0658e93a925aa9a933edfa
yko155375-code <yko155375@gmail.com>
2026-06-02 07:02:01 +0800

    Add v2 status effect presets

---
+++ v2/modules/status-effects.js
@@ -0,0 +1,17 @@
+export const statusEffectGroups = {
+  buffs: [
+    { id: "blessed", label: "祝福" },
+    { id: "guarded", label: "守護" },
+    { id: "inspired", label: "振奮" },
+  ],
+  debuffs: [
+    { id: "hidden", label: "隱藏" },
+    { id: "vulnerable", label: "脆弱" },
+    { id: "restrained", label: "束縛" },
+    { id: "dazed", label: "眩暈" },
+    { id: "frightened", label: "驚懼" },
+    { id: "charmed", label: "迷醉" },
+    { id: "burning", label: "點燃" },
+    { id: "poisoned", label: "中毒" },
+  ],
+};
